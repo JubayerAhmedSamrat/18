b62d6c8edb94c59819d094099df61f00ac60d74b
JubayerAhmedSamrat <jasamrat@gmail.com>
2024-10-24 20:53:47 +0600

    2nd

--- js/takaPoisa.js
+++ js/takaPoisa.js
@@ -1,10 +1,49 @@
+let totalTk = parseInt(document.getElementById('totalPara').innerText);
+
 document.getElementById('deposit').addEventListener('click', function(){
-    const depositIn = parseInt(document.getElementById('depositIn').value);
-    const depositPara = parseInt(document.getElementById('depositPara').innerText);
-    document.getElementById('depositPara').innerText = depositIn + depositPara;
     
-    let totalTk = parseInt(document.getElementById('totalPara').innerText);
-    totalTk += depositIn;
+    const depositInput = parseInt(document.getElementById('depositIn').value);
+    if(isNaN(depositInput)){
+        document.getElementById('deposit').setAttribute('disabled', true);
+        document.getElementById('deposit').classList.remove('hover:bg-blue-700');
+
+    } else{
+        document.getElementById('deposit').removeAttribute('disabled');
+        document.getElementById('deposit').classList.add('hover:bg-blue-700');
+        
+    }
+    if(!isNaN(depositInput)){
+        const depositPara = parseInt(document.getElementById('depositPara').innerText);
+    document.getElementById('depositPara').innerText = depositInput + depositPara;
+    
+    
+    totalTk += depositInput;
     document.getElementById('totalPara').innerText = totalTk;
+    document.getElementById('depositIn').value = '';
+    }
+
+})
+
+document.getElementById('withdraw').addEventListener('click', function (){
+   
+    const withdrawInput = parseInt(document.getElementById('withdrawIn').value);
+    if(isNaN(withdrawInput)){
+        document.getElementById('withdraw').setAttribute('disabled');
+    } else{
+        document.getElementById('withdraw').removeAttribute('disabled');
+        
+    }
+    const withdrawPara = parseInt(document.getElementById('withdrawPara').innerText);
+
+    if(document.getElementById('withdrawIn').value < totalTk){
+        document.getElementById('withdrawPara').innerText = withdrawInput + withdrawPara;
+    
+        totalTk -= withdrawInput;
+        document.getElementById('totalPara').innerText =totalTk;
+    } else {
+        alert("You don't have sufficient balance");
+    }
+    
+    document.getElementById('withdrawIn').value = '';
 
 })
